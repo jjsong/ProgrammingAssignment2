77b74ef59c354038d5bec85677b83506a7da102e
jjsong <santasong@gmail.com>
2014-04-24 22:29:54 +0900

    Peer Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+# create a special "matrix" object 
+makeCacheMatrix <- function(x = matrix()) {    
+        invX <- NULL
+        set <- function(y) {                  # set the matrix 
+                x <<- y
+                invX <<- NULL
+        }
+        get <- function() x                   # get the matrix 
+        setInverse<- function(inverse) invX <<-inverse  # set inverse
+        getInverse <- function() invX         # get inverse
+        list(set = set, get = get,            # return a list with 4 list items
+             setInverse = setInverse,         # matrix
+             getInverse = getInverse)
 }
 
+### usage
+a <- makeCacheMatrix(matrix(1:4,2))
+a$get()
+a$getInverse()
+a$set(matrix(5:8,2))
+a$get()
 
-## Write a short comment describing this function
+?solve()
 
+# calculage the inverse of the special "matrix" returned by makeCacheMatrix()
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        invX <- x$getInverse()
+        if (!is.null(invX)) {                # if there is a cache 
+                message("getting cached inverse special matrix") # print message
+                return(invX)                 # just return it in the cache
+        }                                    # (no computation)          
+        invX <- solve(x$get(), ...)          # if not, compute it here
+        x$setInverse(invX)                   # save the result to x's cache
+        return(invX)                         # return the result
+        
 }
+
+
+### usage : 
+cacheSolve(a)
+cacheSolve(a)
+a$getInverse()
+b = a$getInverse()
+a$get() %*% b     #matrix multiplication should show identity matrix
+
+
+
+
+
